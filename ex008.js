e28f5507082e951d8c7b376d2b89ceaa35b26c39
djh0w <joao.ps.junior1989@gmail.com>
2022-03-08 16:58:39 -0300

    Update Condicoes Simples

---
+++ ex008.js
@@ -0,0 +1,7 @@
+var vel = 50
+console.log(`A velocidade do seu carro é ${vel} por hora`)
+
+if(vel > 60){ //Condicao simples
+    console.log('Voce vai ser multado por excesso de velocidade')
+}
+console.log('Dirija usando o cinto de segurança.')
